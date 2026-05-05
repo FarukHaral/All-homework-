540615cc9e153fced364c69cc2aab8318673ece7
FarukHaral <haral.faruk@ibu.edu.mk>
2026-05-05 08:12:32 +0000

    CS50 Week 1 ödevleri tamamlandı

---
+++ pset1/cash/cash.c
@@ -0,0 +1,13 @@
+#include <cs50.h>
+#include <stdio.h>
+int main(void) {
+    int cents;
+    do { cents = get_int("Change owed: "); } while (cents < 0);
+    int coins = 0;
+    int denominations[] = {25, 10, 5, 1};
+    for (int i = 0; i < 4; i++) {
+        coins += cents / denominations[i];
+        cents %= denominations[i];
+    }
+    printf("%i\n", coins);
+}
